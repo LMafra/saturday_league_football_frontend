56d2c9efb605e6eae50d04731863c1f83380151f
LMafra <chagas.lucas.mafra@gmail.com>
2025-11-27 17:26:39 -0300

    fix lint

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,7 +2,8 @@ import "@testing-library/jest-dom/vitest";
 
 // Polyfill for HTMLFormElement.prototype.requestSubmit (not implemented in jsdom)
 if (!HTMLFormElement.prototype.requestSubmit) {
-  HTMLFormElement.prototype.requestSubmit = function(submitter?: HTMLElement | null) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  HTMLFormElement.prototype.requestSubmit = function(_submitter?: HTMLElement | null) {
     const submitEvent = new Event('submit', { cancelable: true, bubbles: true });
     if (this.dispatchEvent(submitEvent)) {
       this.submit();
